075bbe5c4c85d4492287ce55c5ec9950840c2a70
racaljk <1948638989@qq.com>
2018-12-03 14:49:33 +0800

    lambda: write type for teammates...

--- src/main/java/VideohubLauncher.java
+++ src/main/java/VideohubLauncher.java
@@ -2,11 +2,13 @@ import core.Sparrow;
 import mvc.Router;
 import mvc.View;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class VideohubLauncher {
     public static void main(String[] args) {
-        Router.get("/hello", (req, resp) -> {
+        Router.get("/hello", (HttpServletRequest req, HttpServletResponse resp) -> {
             try {
                 resp.getWriter().println("<p color=\"blue\">Hello videohub</p>");
             } catch (IOException e) {
