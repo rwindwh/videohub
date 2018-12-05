aaa1779dd43149ab0f5ea89b40c3eb671db10212
h2no3 <yangyijk@126.com>
2018-12-05 20:53:05 +0800

    更新sparrow版本

--- src/main/java/router/IndexRouter.java
+++ src/main/java/router/IndexRouter.java
@@ -2,17 +2,10 @@ package router;
 
 import mvc.Router;
 
-import java.io.IOException;
-
 public class IndexRouter implements Registrable {
     @Override
     public void registerRouter() {
         Router.get("/index", (req, resp) -> {
-            try {
-                resp.getWriter().println("index page");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         });
     }
 }
--- src/main/java/router/LoginRouter.java
+++ src/main/java/router/LoginRouter.java
@@ -2,13 +2,10 @@ package router;
 
 import database.DBTemplate;
 import mvc.Router;
+import tool.CaptchaGenerator;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class LoginRouter implements Registrable {
@@ -18,55 +15,17 @@ public class LoginRouter implements Registrable {
     @Override
     public void registerRouter() {
         Router.get("/checkcode", (request, response) -> {
+            CaptchaGenerator cg = CaptchaGenerator.simpleCaptcha(4);
+            request.getSession().setAttribute("checkcode", cg.getCaptchaCode());
+
             try {
-                request.setCharacterEncoding("UTF-8");
-                response.setContentType("image/jpeg");
-                HttpSession session=request.getSession();
-                int width=60;
-                int height=20;
-                response.setHeader("Pragma","No-cache");
-                response.setHeader("Cache-Control","no-cache");
-                response.setDateHeader("Expires",0);
-                BufferedImage image=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-                Graphics g=image.getGraphics();
-                String chars="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                char[] rands=new char[4];
-                for(int i=0;i<4;i++)
-                {
-                    int rand=(int)(Math.random()*36);
-                    rands[i]=chars.charAt(rand);
-                }
-                g.setColor(new Color(0xDCDCDC));
-                g.fillRect(0,0,width,height);
-                for(int i=0;i<120;i++)
-                {
-                    int x=(int)(Math.random()*width);
-                    int y=(int)(Math.random()*height);
-                    int red=(int)(Math.random()*255);
-                    int green=(int)(Math.random()*255);
-                    int blue=(int)(Math.random()*255);
-                    g.setColor(new Color(red,green,blue));
-                    g.drawOval(x,y,1,0);
-                }
-                g.setColor(Color.BLACK);
-                g.setFont(new Font(null,Font.ITALIC|Font.BOLD,18));
-                g.drawString(""+rands[0],1,17);
-                g.drawString(""+rands[1],16,15);
-                g.drawString(""+rands[2],31,18);
-                g.drawString(""+rands[3],46,16);
-                g.dispose();
-                ServletOutputStream sos=response.getOutputStream();
-                ByteArrayOutputStream baos=new ByteArrayOutputStream();
-                ImageIO.write(image,"JPEG",baos);
-                byte[] buffer=baos.toByteArray();
-                response.setContentLength(buffer.length);
-                sos.write(buffer);
-                baos.close();
+                ServletOutputStream sos = response.getOutputStream();
+                sos.write(cg.getByteImage());
                 sos.close();
-                session.setAttribute("checkcode",new String(rands));
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         });
         Router.post("/changecheckcode",(request,reponse)->{
             try{
@@ -82,6 +41,7 @@ public class LoginRouter implements Registrable {
                 String password=request.getParameter("password");
                 HttpSession session=request.getSession();
                 String severcheckcode=(String)session.getAttribute("checkcode");
+
                 DBTemplate.queryOne("select username,password from videohub_user where username='"+username+"'",result->{
                     username1= result.getString("username");
                     password1= result.getString("password");
